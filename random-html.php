a31976350e48cc74e2e450cfeab015fe3ee9ba7d
eliot <louet.eliot50@gmail.com>
2021-09-19 20:27:49 +0200

    init

---
+++ random-html.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Random HTML
+ * Plugin URI: https://github.com/loueteliot/random-html
+ * Description: Display a random HTML from list.
+ * Version: 1.0
+ * Author: Louet Eliot
+ * Author URI: https://github.com/loueteliot
+ */
+?>
